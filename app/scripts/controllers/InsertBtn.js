d39b20727f1a301486e0cd56aa37d8db561c689a
Yukang Yang <zjhzyyk@gmail.com>
2013-05-23 23:27:19 +0800

    fix left issues in insertBtn.js caused by rm excessive files

--- app/scripts/controllers/InsertBtn.js
+++ app/scripts/controllers/InsertBtn.js
@@ -1,16 +1,11 @@
 'use strict';
 
 angular.module('slidesGeneratorApp')
-  .controller('InsertBtnCtrl', ['$scope', 'insertItem', function ($scope, insertItem) {
+  .controller('InsertBtnCtrl', ['$scope', function ($scope) {
     $scope.items = [
 	    "frame",
 	    "textbox"
 	  ];
 	  $scope.click = function(event, item) {
-	  	insertItem.visible = true;
-	  	insertItem.startx = event.screenX;
-	  	insertItem.starty = event.screenY;
-	  	insertItem.type = item;
-	  	console.log(insertItem);
 	  };
   }]);
